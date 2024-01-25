956f92ad989e10346ad9ae96930c541eff0778e1
Cassi <120105652+CypherO2@users.noreply.github.com>
2024-01-25 13:30:59 +0000

    efsf

--- React-Web-Files/src/components/MenuCards.tsx
+++ React-Web-Files/src/components/MenuCards.tsx
@@ -2,8 +2,9 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
 import plus from "../assets/icons8-plus-48.png";
+import { Row, Col } from "react-bootstrap";
 
-interface MenuCardProps {
+export interface MenuCardProps {
   item: string;
   type: string;
   desc: string;
@@ -13,21 +14,29 @@ interface MenuCardProps {
 
 function MenuCard({ item, type, desc, price, url }: MenuCardProps) {
   return (
-    <Card className="my-4 mx-3" bg="dark" style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={url} />
+    <Card className="my-4 mx-4" bg="dark" style={{ width: "18rem" }}>
+      <Row className="">
+        <Card.Img variant="top" src={url} height="250" width="1000" />
+      </Row>
       <Card.Body>
-        <Card.Title className="text-light fw-bold">{item}</Card.Title>
-        <Card.Text className="text-light">{desc}</Card.Text>
-        <h4 className="text-light">
-          {"£"}
-          {price}{" "}
-          <Button
-            className="text-light align-middle fw-bold"
-            variant="outline-dark"
-          >
-            <Image src={plus} rounded />
-          </Button>
-        </h4>
+        <Row className="">
+          <Card.Title className="text-light fw-bold">{item}</Card.Title>
+        </Row>
+        <Row className="">
+          <Card.Text className="text-light">{desc}</Card.Text>
+        </Row>
+        <Row className="pt-4 align-self-bottom">
+          <h4 className="text-light">
+            {"£"}
+            {price.toFixed(2)}{" "}
+            <Button
+              className="text-light align-middle fw-bold "
+              variant="outline-dark"
+            >
+              <Image src={plus} roundedCircle />
+            </Button>
+          </h4>
+        </Row>
       </Card.Body>
     </Card>
   );
--- React-Web-Files/src/pages/Menu.tsx
+++ React-Web-Files/src/pages/Menu.tsx
@@ -1,34 +1,57 @@
 import React, { useEffect, useState } from "react";
-import MenuCard from "../components/MenuCards";
+import MenuCard, { MenuCardProps } from "../components/MenuCards";
 import axios from "axios";
 import { MENU_URL } from "../constants/APIconstants";
-
+import { Col, Row } from "react-bootstrap";
 
 function Menu() {
+  const [menuCards, setMenuCards] = useState([]);
   useEffect(() => {
     document.title = "Bean & Brew | Menu";
   });
   useEffect(() => {
     const getMenu = async () => {
       const response = await axios.get(MENU_URL);
+      console.log(response?.data);
       const menuItems = response?.data["menuItems"];
       console.log(menuItems);
-      menuItems.map((menuItem: any) => {
-      <MenuCard item={menuItem["name"]} type={menuItem["type"]} desc={menuItem["desc"]} price={menuItem["price"]} url={menuItem["url"]} />
-    });
+      setMenuCards(
+        menuItems.map((menuItem: MenuCardProps) => (
+          <MenuCard
+            key={menuItem.item}
+            item={menuItem.item}
+            type={menuItem.type}
+            desc={menuItem.desc}
+            price={menuItem.price}
+            url={menuItem.url}
+          />
+        ))
+      );
+      console.log(menuCards);
+    };
     getMenu();
   }, []);
 
   return (
     <>
-      <MenuCard
-        item="yudsfg"
-        type="drink"
-        desc="lfjsfs;odihfsdfsdsfdsfsdfsdfsdfsfsfsdfdfgrdghdfdkuhf"
-        price={2.32}
-        url="https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_16:9/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2021-11-Pepperoni-Bread%2Fpepperoni-bread-1"
-      />
+      <div className="mx-5 px-5">
+        <Col className="mx-5">
+          <Row fluid>{menuCards}</Row>
+        </Col>
+      </div>
     </>
   );
 }
 export default Menu;
+
+/*
+  const buttonTags = buttons.map((button) => (
+    <button
+      type="button"
+      key={button}
+      onClick={() => handleButtonClick(button)}
+    >
+      {button}
+    </button>
+  ));
+*/
