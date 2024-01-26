b22996481b0cef58a0e3d5871f684e90503caaba
Cassi <120105652+CypherO2@users.noreply.github.com>
2024-01-26 12:48:36 +0000

    fldukh

--- React-Web-Files/src/pages/Contacts.tsx
+++ React-Web-Files/src/pages/Contacts.tsx
@@ -1,9 +1,15 @@
 import { useEffect } from "react";
+import { Col } from "react-bootstrap";
 
 function Contacts(){
       useEffect(() => {
         document.title = "Bean & Brew | Contacts";
       });
-    return <></>
+    return (
+      <>
+        <h1 className="my-3 fw-bold text-center">Contact Us</h1>
+        <Col className="border-bottom my-3"></Col>
+      </>
+    );
 }
 export default Contacts
